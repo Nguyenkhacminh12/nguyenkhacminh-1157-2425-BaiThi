d9c70d4fb326b775abc46bb8ec4582d624f15a70
Nguyenkhacminh12 <nguyenkhacminh995@gmail.com>
2025-01-03 01:11:26 +0700

    CREATE_PROJECT

---
+++ nguyenkhacminh-1157-2425-BaiThi/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace nguyenkhacminh_1157_2425_BaiThi.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
